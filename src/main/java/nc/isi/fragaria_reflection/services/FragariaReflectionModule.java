e690d0001dbc39f128231c0fc56847208e468b2a
jmaltat <jmaltat@isi.nc>
2013-01-30 11:14:58 +1100

    déplacement de ResourceFinder

--- src/main/java/nc/isi/fragaria_reflection/services/FragariaReflectionModule.java
+++ src/main/java/nc/isi/fragaria_reflection/services/FragariaReflectionModule.java
@@ -5,6 +5,7 @@ import org.apache.tapestry5.ioc.ServiceBinder;
 public class FragariaReflectionModule {
 	public static void bind(ServiceBinder binder) {
 		binder.bind(ReflectionFactory.class, ReflectionFactoryImpl.class);
+		binder.bind(ResourceFinder.class, ResourceFinderImpl.class);
 	}
 
 }
