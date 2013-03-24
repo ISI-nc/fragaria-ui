7ecfb77fa6b720d6cd2653a1337779b5e30d04b3
brounchy <benoit.jonathas@gmail.com>
2013-03-25 10:19:54 +1100

    added ability to edit summary

--- src/main/java/nc/isi/fragaria_ui/utils/events/journal/JournalCreateElementEvent.java
+++ src/main/java/nc/isi/fragaria_ui/utils/events/journal/JournalCreateElementEvent.java
@@ -1,11 +1,16 @@
 package nc.isi.fragaria_ui.utils.events.journal;
 
 import nc.isi.fragaria_ui.utils.journal.classes.JournalElement;
+import nc.isi.fragaria_ui.utils.journal.classes.JournalGroup;
 
 public class JournalCreateElementEvent extends AbstractJournalEvent<JournalElement>{
 
-	public JournalCreateElementEvent(JournalElement elt) {
+	private final JournalGroup group;
+	
+	public JournalCreateElementEvent(JournalElement elt,
+			JournalGroup group) {
 		super(elt);
+		this.group = group;
 	}
 
 }
