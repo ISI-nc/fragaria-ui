be89644d630f98eb576f5f829a2f0380e7a9a3aa
brounchy <benoit.jonathas@gmail.com>
2013-06-04 10:36:53 +1100

    Removed Form from AutocompletionSearch

--- src/main/java/nc/isi/fragaria_ui/pages/PropertyEditBlocks.java
+++ src/main/java/nc/isi/fragaria_ui/pages/PropertyEditBlocks.java
@@ -7,11 +7,13 @@ import org.apache.tapestry5.FieldTranslator;
 import org.apache.tapestry5.FieldValidator;
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Environmental;
+import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.DateField;
 import org.apache.tapestry5.corelib.components.TextField;
 import org.apache.tapestry5.services.PropertyEditContext;
 
+@Import(module="bootstrap")
 public class PropertyEditBlocks {
 
 	@Property
